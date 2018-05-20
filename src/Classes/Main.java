d878a17cc11c724e81e4de02b57e024da095cb59
Chaphloziferar <johnhectorlargaespada@gmail.com>
2018-05-19 23:14:58 -0600

    Primer Commit

---
+++ src/Classes/Main.java
@@ -0,0 +1,18 @@
+package Classes;
+
+import Ventanas.Intro_Carga;
+
+/**
+ * @author Chaphloziferar
+ **/
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        Intro_Carga ic = new Intro_Carga();
+        ic.setVisible(true);
+        
+    }
+    
+}
---
+++ src/Classes/Pokemon.java
@@ -0,0 +1,64 @@
+package Classes;
+
+/**
+ * @author Chaphloziferar
+ **/
+
+public class Pokemon {
+    
+    private String Name;
+    private int HP;
+    private int Atack1;
+    private int Atack2;
+    private int Atack3;
+    private int Atack4;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getHP() {
+        return HP;
+    }
+
+    public void setHP(int HP) {
+        this.HP = HP;
+    }
+
+    public int getAtack1() {
+        return Atack1;
+    }
+
+    public void setAtack1(int Atack1) {
+        this.Atack1 = Atack1;
+    }
+
+    public int getAtack2() {
+        return Atack2;
+    }
+
+    public void setAtack2(int Atack2) {
+        this.Atack2 = Atack2;
+    }
+
+    public int getAtack3() {
+        return Atack3;
+    }
+
+    public void setAtack3(int Atack3) {
+        this.Atack3 = Atack3;
+    }
+
+    public int getAtack4() {
+        return Atack4;
+    }
+
+    public void setAtack4(int Atack4) {
+        this.Atack4 = Atack4;
+    }
+    
+}
